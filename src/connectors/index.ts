1a7fe4a2645fb57a681dcd37300ca494b1bd2471
weidaix <weidai@inbox.ru>
2021-04-08 11:51:42 -0400

    support for BSC wallet

--- src/connectors/index.ts
+++ src/connectors/index.ts
@@ -3,6 +3,7 @@ import { NetworkConnector } from "@web3-react/network-connector"
 import { WalletConnectConnector } from "@web3-react/walletconnect-connector"
 import { WalletLinkConnector } from "@web3-react/walletlink-connector"
 import { Web3Provider } from "@ethersproject/providers"
+import { BscConnector } from "@binance-chain/bsc-connector"
 
 const NETWORK_URL = process.env.REACT_APP_NETWORK_URL
 export const NETWORK_CHAIN_ID: number = parseInt(
@@ -30,6 +31,10 @@ export function getNetworkLibrary(): Web3Provider {
 export const injected = new InjectedConnector({
   // mainnet, ropsten, rinkeby, goerli, kovan, local buidler
   // see: https://github.com/ethereum/EIPs/blob/master/EIPS/eip-155.md
+  supportedChainIds: [56, 97],
+})
+
+export const bsc = new BscConnector({
   supportedChainIds: [56, 97],
 })
 
