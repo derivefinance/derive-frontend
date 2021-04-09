df38e6fe1e3b6f24a55e56f12238d80d0b7705e4
weidaix <weidai@inbox.ru>
2021-04-08 23:00:59 -0400

    smart contracts info

--- src/pages/Risk.tsx
+++ src/pages/Risk.tsx
@@ -31,18 +31,28 @@ function Risk(): ReactElement {
         <p>{t("riskAdminKeys")}</p>
         <h3>{t("lossOfPeg")}</h3>
         <p>{t("riskLossOfPeg")}</p>
-        <h3>{t("contact")}</h3>
+        <h3>{t("smartContracts")}</h3>
+        <p>{t("smartContractsInfo")}
+        <a href="https://github.com/derivefinance" target="_blank" rel="noreferrer" >
+          {t("github")}{" "}
+        </a>  
+        {t("verified")}{" "}
+        <a href="https://bscscan.com/address/0xA726A51b8Fe5f027ce5039E6400864Fd0BDCB10F#code" target="_blank" rel="noreferrer" > 
+        {t("stablecoin")}{" "}
+        </a>
+        </p>
 
+        <h3>{t("contact")}</h3>
         <p>{t("contactInfo")}  
-        <a href="https://t.me/derivefinance">
+        <a href="mailto:support@derive.fi">
           {t("email")}{" "}
           </a> 
           {t("staffInfo")}  
-        <a href="https://t.me/derivefinance">
+        <a href="https://t.me/derivefinance" target="_blank" rel="noreferrer" >
           {t("telegram")}
           </a> 
           {t("twitter")}{" "}
-          <a href="https://twitter.com/derivefinance">
+          <a href="https://twitter.com/derivefinance" target="_blank" rel="noreferrer">
           {t("twitterInfo")}
           </a>
           </p>          
